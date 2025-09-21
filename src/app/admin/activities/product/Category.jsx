cd2759ed94c9c6159443093c9832167f52450499
abu-omer <90451806+abu-omer@users.noreply.github.com>
2025-09-21 01:56:39 -0700

    fetching categories

--- src/app/admin/activities/product/Category.jsx
+++ src/app/admin/activities/product/Category.jsx
@@ -5,13 +5,12 @@ import axios from "axios";
 
 export default function Category() {
   const [cat, setCat] = useState("");
-  const [isActive, setIsActive] = useState(true);
+  // const [isActive, setIsActive] = useState(true);
   const [categories, setCategories] = useState([]);
 
   const onSubmit = async () => {
     await axios.post("http://localhost:3000/categories", {
       name: cat,
-      isActive,
     });
   };
 
@@ -23,43 +22,6 @@ export default function Category() {
 
   return (
     <div className="container d-flex flex-column mt-5 justify-content-center align-items-center">
-      <form className="d-flex flex-column gap-3 align-items-center justify-content-center">
-        <div className=" d-flex aling-items-center justify-content-between gap-3">
-          <label> Name</label>
-          <Input
-            type="text"
-            className="form-control"
-            id="cat"
-            placeholder="Enter category name (Alphabets)"
-            name="cat"
-            value={cat}
-            onChange={(e) => setCat(e.target.value)}
-          />
-        </div>
-        <div className="d-flex aling-items-center justify-content-between gap-3">
-          <label className="">Status</label>
-          <select
-            className="form-select "
-            style={{ width: "205px" }}
-            aria-label="Default select example"
-            onChange={(e) => setIsActive(e.target.value)}
-          >
-            <option value="">select status</option>;
-            <option value="true">true</option>;
-            <option value="false">false</option>;
-          </select>
-        </div>
-        <Button
-          type="submit"
-          variant="primary"
-          size="medium"
-          className="btn px-5 mt-2"
-          onClick={onSubmit}
-        >
-          ADD
-        </Button>
-      </form>
-
       <div className="container rounded mt-2">
         <table className="table table-bordered">
           <thead>
@@ -89,6 +51,63 @@ export default function Category() {
             ))}
           </tbody>
         </table>
+        <div className="d-flex justify-content-center">
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#myModal"
+          >
+            Add Category
+          </button>
+        </div>
+
+        <div className="modal" id="myModal">
+          <div className="modal-dialog modal-lg">
+            <div className="modal-content justify-content-center text-center ">
+              <h4 className="modal-title mt-3">Add Category</h4>
+
+              <div className="modal-body">
+                <form className="d-flex flex-column gap-3 align-items-center justify-content-center">
+                  <div className=" d-flex aling-items-center justify-content-between gap-3">
+                    <label> Name</label>
+                    <Input
+                      type="text"
+                      className="form-control"
+                      id="cat"
+                      placeholder="Enter category name (Alphabets)"
+                      name="cat"
+                      value={cat}
+                      onChange={(e) => setCat(e.target.value)}
+                    />
+                  </div>
+
+                  <Button
+                    type="submit"
+                    variant="primary"
+                    size="medium"
+                    className="btn px-5 mt-2"
+                    onClick={onSubmit}
+                  >
+                    ADD
+                  </Button>
+                </form>
+              </div>
+
+              <div className="d-flex justify-content-center">
+                <button
+                  type="button"
+                  class="btn btn-danger"
+                  data-bs-dismiss="modal"
+                  className="btn btn-primary mb-2 "
+                  style={{ width: "100px" }}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
