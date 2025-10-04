4a9f67206aff1984df2289ca715dcff97f878f68
abu-omer <90451806+abu-omer@users.noreply.github.com>
2025-10-04 02:53:10 -0700

    Refactor code structure for improved readability and maintainability

--- src/app/user/common/Banner.jsx
+++ src/app/user/common/Banner.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
-
+import banner1 from "../../assits/banner1.jpg";
+import banner2 from "../../assits/banner2.jpg";
+import banner3 from "../../assits/banner3.jpg";
 export default function Banner() {
   return (
-    <div>
-      Banner
+    <div id="demo" class="carousel slide" data-bs-ride="carousel">
+      {/* Indicators/dots */}
+      <div class="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#demo"
+          data-bs-slide-to="0"
+          class="active"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#demo"
+          data-bs-slide-to="1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#demo"
+          data-bs-slide-to="2"
+        ></button>
+      </div>
+      {/* The slideshow/carousel */}
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            src={banner1}
+            alt="Los Angeles"
+            class="d-block"
+            style={{ width: "100%", height: "576px" }}
+          />
+        </div>
+        <div class="carousel-item active">
+          <img
+            src={banner2}
+            alt="Los Angeles"
+            class="d-block"
+            style={{ width: "100%", height: "576px" }}
+          />
+        </div>
+        <div class="carousel-item active">
+          <img
+            src={banner3}
+            alt="Los Angeles"
+            class="d-block"
+            style={{ width: "100%", height: "576px" }}
+          />
+        </div>
+      </div>
+      {/* Left and right controls/icons  */}
+      <div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#demo"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon"></span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#demo"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon"></span>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
--- src/app/user/common/Footer.jsx
+++ src/app/user/common/Footer.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      Footer
-    </div>
-  )
+    <footer className="bg-dark text-white ">
+      <div className="bg-gray align-items-center text-center pt-3 justify-content-cnter">
+        Back To Top
+      </div>
+      <div className="container row mt-5 p-5">
+        <div className="col ms-5">
+          <p>Get to Know Us</p>
+          <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+            <li>About Amazon</li>
+            <li>Careers</li>
+            <li>Amazon Science</li>
+          </ul>
+        </div>
+        <div className="col">
+          <p>Shop with Us</p>
+          <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+            <li>Your Account</li>
+            <li>Your Orders </li>
+            <li>Your Addresses </li>
+            <li>Your Lists </li>
+          </ul>
+        </div>
+        <div className="col">
+          <p>Make Money with Us</p>
+          <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+            <li>Protect and build your brand</li>
+            <li>Sell on Amazon</li>
+            <li>Fulfillment by Amazon</li>
+            <li>Supply to Amazon</li>
+            <li>Become an Affiliate</li>
+            <li>dvertise Your Products</li>
+          </ul>
+        </div>
+        <div className="col">
+          <p>Let Us Help You</p>
+          <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+            <li>Help</li>
+            <li>Shipping & Delivery</li>
+            <li>Returns & Replacements</li>
+            <li>Recalls and Product Safety Alerts</li>
+            <li>Amazon App Download</li>
+          </ul>
+        </div>
+      </div>
+      <div></div>
+    </footer>
+  );
 }
