9bc28d11ba87a652736489da951b3cf5e74b0e4a
abu-umer2 <kh.mohmed97@yahoo.com>
2025-10-07 21:46:15 +0530

    changes 07-10

--- src/app/user/UserLandingPage.jsx
+++ src/app/user/UserLandingPage.jsx
@@ -7,54 +7,6 @@ const UserLandingPage = () => {
   return (
     <div>
       <Header />
-      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapsibleNavbar"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul className="navbar-nav">
-              <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                >
-                  Products
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to="cat">
-                      Category
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="subcat">
-                      Sub category
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="product">
-                      Product
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link" to="logout">
-                  Logout
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
       <Menu />
       <Outlet />
       <Footer />
--- src/app/user/common/Menu.jsx
+++ src/app/user/common/Menu.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import ProductServices from "../../services/productServices";
 export default function Menu() {
+  var [category, updateCat] = useState([]);
+  useEffect(() => {
+    ProductServices.fetchCategories().then(data=>{
+      console.log(data.data);
+    updateCat(data.data);
+    })
+  }, []);
+  function capitalizeFirstLetter(str) {
+  if (typeof str !== 'string' || str.length === 0) {
+    return str; // Return as is for non-string or empty inputs
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
   return (
     <div>
-      Menu
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapsibleNavbar"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul className="navbar-nav">
+              {category.map((obj) => {
+
+                return (
+                  <li className="nav-item dropdown">
+                    <Link
+                      className="nav-link dropdown-toggle"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                    >
+                      {capitalizeFirstLetter(obj.name)}
+                    </Link>
+                    <ul className="dropdown-menu">
+                     
+                      <li>
+                        <Link className="dropdown-item" to="subcat">
+                          Sub category
+                        </Link>
+                      </li>
+                      
+                    </ul>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
