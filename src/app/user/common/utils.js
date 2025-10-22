cc7eca1649d52a745229d7bf10c2ba4cfee1abd3
abu-omer <90451806+abu-omer@users.noreply.github.com>
2025-10-22 11:52:10 -0700

    Add user signup functionality and enhance SignUp component with additional fields

--- src/app/user/common/utils.js
+++ src/app/user/common/utils.js
@@ -3,12 +3,12 @@ import "./common.scss";
 export default {
   ShowDialog(props) {
     const [children, setChildren] = useState(props.childComponent);
-    // function funClose() {
-    //   alert("Called");
-    //   setChildren("");
-    // }
+    function funClose() {
+      alert("Called");
+      setChildren("");
+    }
     return (
-      <div className="modal-overlay ">
+      <div className="modal-overlay">
         <div>{children}</div>
       </div>
     );
