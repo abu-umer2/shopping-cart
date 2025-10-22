c1af3fd2b5836fdad9901e7ea55bc264eca1507d
abu-omer <90451806+abu-omer@users.noreply.github.com>
2025-10-22 07:37:17 -0700

    Implement SignUp component with enhanced user input fields and layout

--- src/app/user/components/SignUp.jsx
+++ src/app/user/components/SignUp.jsx
@@ -1,37 +1,85 @@
 import Input from "../../shared/controls/Input";
 import Button from "../../shared/controls/Button";
+import { useState } from "react";
 const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div className="d-flex justify-content-center align-items-center">
-      <div
-        className=" d-flex flex-column m-5 p-5 p3 gap-4 border  border-2 rounded"
-        style={{ width: "500px", backgroundColor: "white" }}
-      >
-        <div className="d-flex justify-content-between ">
-          <label htmlFor="">First Name:</label>{" "}
-          <Input placeholder="Enter Your first name" />
-        </div>
-
-        <div className="d-flex justify-content-between ">
-          <label htmlFor="">Last Name:</label>{" "}
-          <Input placeholder="Enter Your last name" />
-        </div>
-
-        <div className="d-flex justify-content-between ">
-          <label htmlFor="">Email:</label>{" "}
-          <Input placeholder="Enter Your email" />
-        </div>
-
-        <div className="d-flex justify-content-between ">
-          <label htmlFor="">Username:</label>{" "}
-          <Input placeholder="Enter Your username" />
-        </div>
-        <div className="d-flex justify-content-between ">
-          <label htmlFor="">Password:</label>
-          <Input placeholder="Enter your Password" />
-        </div>
-
-        <Button size="small">Sign Up</Button>
+    <div className=" ">
+      <div className="text-center">
+        <p>Create a new account </p>
+      </div>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <form className="row g-3 p-3  w-50">
+          <div className="col-md-6">
+            <label htmlFor="">Username:</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="" className="form-label">
+              Email:
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="" className="form-label">
+              First Name:
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="" className="form-label">
+              Last Name:
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="" className="form-label">
+              Password:
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="" className="form-label">
+              Phone Number:
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-12">
+            <label htmlFor="" className="form-label">
+              Street:
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-12">
+            <label htmlFor="" className="form-label">
+              City:
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="" className="form-label">
+              State:
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="" className="form-label">
+              Zip Code:
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-2">
+            <label htmlFor="" className="form-label">
+              Country:
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+        </form>
       </div>
     </div>
   );
