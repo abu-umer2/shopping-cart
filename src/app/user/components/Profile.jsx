ca44ed36d324d999b8a4cdf9d81daea624c478fc
abu-omer <90451806+abu-omer@users.noreply.github.com>
2025-11-01 18:31:15 +0400

    Add Profile component and update routing; enhance login to store user data

---
+++ src/app/user/components/Profile.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Profile = () => {
+  const storedUser = sessionStorage.getItem("user");
+  const user = storedUser ? JSON.parse(storedUser) : null;
+
+  console.log("profile", user);
+  return (
+    <div className="containter bg-light">
+      <div className="d-flex flex-column align-items-center justify-content-center gap-2 m-4 border border-2 p-3">
+        <div className="d-flex  justify-content-between w-75">
+          <label htmlFor="">First Name</label>
+          <input type="text" value={user.firstName} />
+        </div>
+        <div className="d-flex justify-content-between w-75">
+          <label htmlFor="">First Name</label>
+          <input type="text" value={user.firstName} />
+        </div>
+        <div className="d-flex justify-content-between w-75">
+          <label htmlFor="">Email</label>
+          <input type="text" value={user.email} />
+        </div>
+        <div className="d-flex justify-content-between w-75">
+          <label htmlFor="">Last Name</label>
+          <input type="text" value={user.lastName} />
+        </div>
+        <div className="d-flex justify-content-between w-75">
+          <label htmlFor="">Phone Number</label>
+          <input type="text" value={user.phoneNumber} />
+        </div>
+        <div className="d-flex justify-content-between w-75">
+          <label htmlFor="">First Name</label>
+          <input type="text" value={user.firstName} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
